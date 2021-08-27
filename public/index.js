b9401dda082ca0c4884900d7fb0b5d16b9828973
Sean Morrissey <semorrissey17@gmail.com>
2021-08-26 22:09:36 -0400

    added borders to navbars for debugging and began debugging visibility

--- public/index.js
+++ public/index.js
@@ -14,3 +14,25 @@ function myFunction() {
     btn.innerHTML = "Play";
   }
 }
+
+/*this doesnt wokr because for some reason the boolean is only true or false not changing*/
+function toggleVisibility(item) {
+  if ($('#' + item.innerHTML).is(":visible")) {
+    console.log("I hidden");
+    $('#' + item.innerHTML).css({
+      'visibility': 'hidden'
+    });
+  } else {
+    console.log($('#' + item.innerHTML).is(":visible"));
+    $('#' + item.innerHTML).css({
+      'visibility': 'visible'
+    });
+  }
+
+
+  /*  if (document.getElementById(item.innerHTML).style.visibility === "visible") {
+      document.getElementById(item.innerHTML).style.visibility = "hidden";
+    } else {
+      document.getElementById(item.innerHTML).style.visibility = "visible";
+    }*/
+}
