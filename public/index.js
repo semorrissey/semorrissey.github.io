e9610a0363743ad2a30a33305cb09af6015a77e1
Sean Morrissey <semorrissey17@gmail.com>
2021-09-23 23:09:56 -0400

    overhauled page layouts to fit bootstrap grid

--- public/index.js
+++ public/index.js
@@ -66,6 +66,7 @@ function toggleVisibility(item) {
 }
 
 
-$(function() {
-  $('[data-toggle="popover"]').popover()
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function(popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
 })
